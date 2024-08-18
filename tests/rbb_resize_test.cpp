00d606705eacc26f28a861ede438ccaa4493d793
copyrat90 <copyrat90@gmail.com>
2024-08-18 21:32:14 +0900

    Add `rbb_resize_test`

---
+++ tests/rbb_resize_test.cpp
@@ -0,0 +1,70 @@
+#include "RingByteBuffer.hpp"
+
+#include <array>
+#include <cstdlib>
+#include <iostream>
+#include <source_location>
+
+#define TEST_ASSERT(condition) \
+    do \
+    { \
+        if (!(condition)) \
+        { \
+            const auto loc = std::source_location::current(); \
+            std::cout << "Failed: " << #condition << "\n\t"; \
+            std::cout << "at " << loc.file_name() << ":" << loc.line() << ":" << loc.column() << "\n"; \
+            std::exit(1); \
+        } \
+    } while (false)
+
+static constexpr std::array<std::byte, 5> HELLO = {
+    std::byte('h'), std::byte('e'), std::byte('l'), std::byte('l'), std::byte('o'),
+};
+
+int main()
+{
+    rbb::RingByteBuffer ring;
+
+    std::byte temp = std::byte(0);
+    std::array<std::byte, 5> temp_5;
+
+    TEST_ASSERT(!ring.try_resize(0));
+    TEST_ASSERT(!ring.data());
+
+    TEST_ASSERT(ring.try_resize(1));
+    TEST_ASSERT(ring.data());
+    TEST_ASSERT(ring.try_write(HELLO.data(), 1));
+    TEST_ASSERT(!ring.try_resize(0));
+
+    TEST_ASSERT(!ring.try_read(&temp, 2));
+    TEST_ASSERT(temp == std::byte(0));
+    TEST_ASSERT(ring.try_read(&temp, 1));
+    TEST_ASSERT(temp == HELLO[0]);
+
+    TEST_ASSERT(ring.try_write(HELLO.data() + 1, 1));
+    TEST_ASSERT(ring.try_resize(2));
+    TEST_ASSERT(ring.try_read(&temp, 1));
+    TEST_ASSERT(temp == HELLO[1]);
+    TEST_ASSERT(ring.try_resize(0));
+    TEST_ASSERT(ring.empty());
+    TEST_ASSERT(!ring.data());
+    TEST_ASSERT(ring.available_space() == 0);
+
+    TEST_ASSERT(ring.try_resize(sizeof(HELLO)));
+    TEST_ASSERT(ring.try_write(HELLO.data(), sizeof(HELLO)));
+    TEST_ASSERT(ring.try_read(temp_5.data(), sizeof(temp_5)));
+    TEST_ASSERT(temp_5 == HELLO);
+    TEST_ASSERT(ring.try_write(HELLO.data(), sizeof(HELLO)));
+    TEST_ASSERT(ring.consecutive_read_length() == 1);
+    TEST_ASSERT(ring.consecutive_write_length() == 0);
+    TEST_ASSERT(ring.try_read(temp_5.data(), 3));
+    TEST_ASSERT(ring.consecutive_read_length() == 2);
+    TEST_ASSERT(ring.consecutive_write_length() == 2);
+    TEST_ASSERT(ring.available_space() == 3);
+    TEST_ASSERT(ring.used_space() == 2);
+    TEST_ASSERT(ring.try_read(temp_5.data() + 3, 2));
+    TEST_ASSERT(temp_5 == HELLO);
+    TEST_ASSERT(ring.empty());
+
+    std::cout << "All is well!" << std::endl;
+}
